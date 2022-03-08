75b93ab77a24f22c30a5ac749cf4fde52fbd0842
egberts <egberts@github.com>
2022-03-08 15:44:28 -0500

    Added support script for initial starting of this script

---
+++ examples/tls-ca-create-subdirs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+GROUP_SSL_CERT='ssl-cert'
+
+echo "Creates the /etc/ssl subdirectories as being "
+echo "owned by the 'ssl-cert' group"
+echo
+THIS_USER="${THIS_USER:-$1}"
+
+# count how many users have `ssl-cert` group
+group_ssl_cert_count="$(grep -c "$GROUP_SSL_CERT" /etc/group)"
+
+echo "Users with '${GROUP_SSL_CERT}' group: $group_ssl_cert_count"
+
+if [ $group_ssl_cert_count -ge 2 ]; then
+  echo "Ummmm, I haven't exactly mastered multi-users yet."
+  echo "Aborted."
+  echo
+elif [ $group_ssl_cert_count -eq 1 ]; then
+  echo "Using '$THIS_USER' as sole operator of '${GROUP_SSL_CERT}' group"
+  echo
+fi
+
+function test_and_mkdir()
+{
+  local this_dir="$1"
+  local expected_user="$2"
+  local expected_group="$3"
+  local expected_mod="$4"
+  COMPLAIN=0
+  if [ -e "$this_dir" ]; then
+    echo "Directory $this_dir is already created."
+    # Checking if file ownership is good
+    file_user="$(stat -c %U "$this_dir")"
+    if [ "$file_user" != "$expected_user" ]; then
+      echo "  Actual file user ID  : $file_user"
+      echo "  Expected file user ID: $expected_user"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_user' user."
+    fi
+    file_group="$(stat -c %G "$this_dir")"
+    if [ "$file_group" != "$expected_group" ]; then
+      echo "  Actual file group ID  : $file_group"
+      echo "  Expected file group ID: $expected_group"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_group' group."
+    fi
+    # Checking if file permission is good
+    file_mod="$(stat -c %a "$this_dir")"
+    if [ "$file_mod" != "$expected_mod" ]; then
+      echo "  Actual file permission: $file_mod"
+      echo "  Expected file permission: $expected_mod"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_mod' file permission."
+    fi
+  else
+    echo "Creating $this_dir directory ..."
+    mkdir "$this_dir"
+    echo "Setting $this_dir directory to ${expected_user}:${expected_group} file ownership ..."
+    chown "${expected_user}:${expected_group}" "$this_dir"
+    echo "Setting $this_dir directory to $expected_mod file permission ..."
+    chmod "$expected_mod" "$this_dir"
+  fi
+}
+
+test_and_mkdir "/etc/ssl/ca"  "${THIS_USER}" "${GROUP_SSL_CERT}" "750"
+test_and_mkdir "/etc/ssl/etc" "${THIS_USER}" "${GROUP_SSL_CERT}" "750"
+test_and_mkdir "/etc/ssl/crl" "${THIS_USER}" "${GROUP_SSL_CERT}" "750"
+echo
+
+echo "Done."
+
---
+++ examples/tls-ca-validate-subdirs.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo "Validates if the /etc/ssl subdirectories are adequately"
+echo "owned by 'ssl-cert' group"
+echo
+echo "No changes shall be done by this script."
+echo "Passive verification only"
+echo
+
+# count how many users have `ssl-cert` group
+group_ssl_cert_count="$(grep -c ssl-cert /etc/group)"
+
+echo "Users with 'ssl-cert' group: $group_ssl_cert_count"
+echo
+
+if [ $group_ssl_cert_count -ge 2 ]; then
+  echo "Ummmm, I haven't exactly mastered the support for multi-users yet."
+  echo
+elif [ $group_ssl_cert_count -eq 0 ]; then
+  echo "INFO: Nobody is in 'ssl-cert' UNIX group"
+  echo "      To add a user to the 'ssl-cert' supplemental group, execute:"
+  echo
+  echo "    usermod -a -G ssl-cert ${USER}"
+  echo
+  echo "After the above command is done, either logout/relogin or "
+  echo "do a new 'ssh localhost' login to get the new "
+  echo "'ssl-cert' supplemental group."
+  echo
+fi
+
+function test_and_verify_subdirs()
+{
+  local this_dir="$1"
+  local expected_user="$2"
+  local expected_group="$3"
+  local expected_mod="$4"
+  COMPLAIN=0
+  if [ -e "$this_dir" ]; then
+    echo "Directory $this_dir is already created."
+    # Checking if file ownership is good
+    file_user="$(stat -c %U "$this_dir")"
+    if [ "$file_user" != "$expected_user" ]; then
+      echo "  Actual file user ID  : $file_user"
+      echo "  Expected file user ID: $expected_user"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_user' user."
+    fi
+    file_group="$(stat -c %G "$this_dir")"
+    if [ "$file_group" != "$expected_group" ]; then
+      echo "  Actual file group ID  : $file_group"
+      echo "  Expected file group ID: $expected_group"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_group' group."
+    fi
+    # Checking if file permission is good
+    file_mod="$(stat -c %a "$this_dir")"
+    if [ "$file_mod" != "$expected_mod" ]; then
+      echo "  Actual file permission: $file_mod"
+      echo "  Expected file permission: $expected_mod"
+      COMPLAIN=1
+    else
+      echo "  Correctly set at '$file_mod' file permission."
+    fi
+  else
+    echo "Missing $this_dir directory ..."
+  fi
+}
+
+test_and_verify_subdirs "/etc/ssl/ca"  "${USER}" "ssl-cert" "750"
+test_and_verify_subdirs "/etc/ssl/etc" "${USER}" "ssl-cert" "750"
+test_and_verify_subdirs "/etc/ssl/crl" "${USER}" "ssl-cert" "750"
+echo
+
+echo "${BASH_SOURCE[0]}: Done."
+
